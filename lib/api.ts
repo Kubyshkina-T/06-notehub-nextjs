46c8316f07725ebafe3351d6af1a48143392bfd9
Tetiana Kubyshkina <tanyasha_@ukr.net>
2026-04-16 16:16:53 +0300

    feat: upload header, footer

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import type { Note} from "../types/note";
+import type { Note} from "@/types/note";
 import type { FormValues } from "../components/NoteForm/NoteForm";
 
 export interface NotesResponse {
@@ -9,7 +9,7 @@ export interface NotesResponse {
 axios.defaults.baseURL = "https://notehub-public.goit.study/api";
 
 axios.defaults.headers.common["Authorization"] =
-  `Bearer ${import.meta.env.VITE_NOTEHUB_TOKEN}`;
+  `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`;
     
 export const fetchNotes = async (
   page: number = 1,
