9ee7998472152902cae921bc35a3012b7490d237
Tetiana Kubyshkina <tanyasha_@ukr.net>
2026-04-16 15:36:28 +0300

    feat: created homepage

---
+++ lib/api.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import type { Note} from "../types/note";
+import type { FormValues } from "../components/NoteForm/NoteForm";
+
+export interface NotesResponse {
+  notes: Note[];
+  totalPages: number;
+}
+axios.defaults.baseURL = "https://notehub-public.goit.study/api";
+
+axios.defaults.headers.common["Authorization"] =
+  `Bearer ${import.meta.env.VITE_NOTEHUB_TOKEN}`;
+    
+export const fetchNotes = async (
+  page: number = 1,
+  perPage: number = 12,
+  searchQuery: string = ""
+): Promise<NotesResponse> => {
+  const res = await axios.get<NotesResponse>("/notes", {
+    params: {
+      page,
+      perPage,
+      search: searchQuery,
+    },
+  });
+
+  return res.data;
+};
+
+export const createNote = async (note: FormValues): Promise<Note> => {
+  const res = await axios.post<Note>("/notes", note);
+  return res.data;
+};
+
+export const deleteNote = async (postId: string): Promise<Note> => {
+  const res = await axios.delete<Note>(`/notes/${postId}`);
+  return res.data;
+};
